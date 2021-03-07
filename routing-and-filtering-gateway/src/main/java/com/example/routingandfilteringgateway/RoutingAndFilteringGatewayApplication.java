82a919f25c8c45509c873ad762d06e564de97ad9
Lakshyajit <lakshyajit165@gmail.com>
2021-03-07 19:01:22 +0530

    spring cloud gateway working with other microservices

---
+++ routing-and-filtering-gateway/src/main/java/com/example/routingandfilteringgateway/RoutingAndFilteringGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.example.routingandfilteringgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class RoutingAndFilteringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingAndFilteringGatewayApplication.class, args);
+	}
+
+
+}
+
+
